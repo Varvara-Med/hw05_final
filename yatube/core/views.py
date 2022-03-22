235f9a5e9b24d497a4ca012f83ec9fff3ed6a17c
Varvara-Med <90317295+Varvara-Med@users.noreply.github.com>
2022-03-22 12:54:38 +0200

    Update views.py

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    # Переменная exception содержит отладочную информацию;
+    """Переменная exception содержит отладочную информацию."""
     return render(request, 'core/404.html', {'path': request.path},
                   status=HTTPStatus.NOT_FOUND)
 
